c653ae35157ff9f712060d2b25d3da7c31892794
wuchen <lookwuchen@outlook.com>
2025-07-24 23:02:27 +0800

    Initial commit

---
+++ demo/invoke/ollama_invoke.py
@@ -0,0 +1,13 @@
+import ollama
+# 获得客户端对象
+client = ollama.Client(host = 'http://localhost:11434')
+
+# print(client.list())
+# print(client.show("deepseek-r1:1.5b"))
+# print(client.ps())
+# 模型对话
+response = client.chat(
+    model = 'deepseek-r1:1.5b',
+    messages = [{"role":"user","content":"你是谁"}]
+)
+print(response['message']['content'])
